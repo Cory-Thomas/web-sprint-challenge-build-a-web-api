61f6ed03e7e1c63e77a318bb5e1493da7e9c1251
Cory Thomas <cory.a.thomas1992@gmail.com>
2020-10-02 11:41:59 -0400

    met mvp for the project routes

--- routers/projectRouter.js
+++ routers/projectRouter.js
@@ -13,4 +13,35 @@ router.get( '/', ( req, res ) => {
         })
 });
 
+router.post( '/', ( req, res ) => {
+    Projects.insert( req.body )
+        .then( newProject => {
+            res.status( 201 ).json({ newProject });
+        })
+        .catch( error => {
+            res.status( 500 ).json({ error: "could not post new project" });
+        })
+});
+
+router.put( '/:id', ( req, res ) => {
+    Projects.update( req.params.id, req.body )
+        .then( updatedResource => {
+            res.status( 200 ).json({ updatedResource });
+        })
+        .catch( error => {
+            res.status( 500 ).json({ error: "could not update project" });
+        })
+});
+
+router.delete( "/:id", ( req, res ) => {
+    Projects.remove( req.params.id )
+        .then( numberRemoved => {
+            res.status( 200 ).json({ numberRemoved });
+        })
+        .catch( error => {
+            res.status( 500 ).json({ error: "could not delete project" });
+        })
+});
+
+
 module.exports = router;
