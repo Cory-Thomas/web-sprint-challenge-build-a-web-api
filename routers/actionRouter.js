75c70ea41d7e1a7fe00a037defe6aa37fc18468a
Cory Thomas <cory.a.thomas1992@gmail.com>
2020-10-02 11:37:44 -0400

    finished the mvp for action routes

--- routers/actionRouter.js
+++ routers/actionRouter.js
@@ -14,4 +14,38 @@ router.get( '/:id', ( req, res ) => {
         })
 });
 
+router.post( '/:id', ( req, res ) => {
+    Actions.insert( req.body )
+        .then( newAction => {
+            res.status( 200 ).json({ message: "action added!", data: newAction });
+        })
+        .catch(error => {
+            res.status( 500 ).json({ error: "could not add action" });
+        })
+});
+
+router.put( '/:id/:actionid', ( req, res ) => {
+    Actions.update( req.params.actionid, req.body )
+        .then( updatedAction => {
+            if( updatedAction !== null ){
+                res.status( 200 ).json({ updatedAction });
+            } else {
+                res.status( 500 ).json({ error: "the action does not exist!" });
+            }
+        })
+        .catch( error => {
+            res.status( 500 ).json({ error: "could not update action" });
+        })
+});
+
+router.delete( '/:id/:actionid', ( req, res ) => {
+    Actions.remove( req.params.actionid )
+        .then( numberRemoved => {
+            res.status( 200 ).json({ numberRemoved });
+        })
+        .catch(error => {
+            res.status( 500 ).json({ error: "could not delete action" });
+        })
+});
+
 module.exports = router;
